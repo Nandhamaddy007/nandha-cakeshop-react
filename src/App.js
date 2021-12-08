38a66aa29adb923e7e232bb50763145a833f8ae5
Nandhamaddy007 <nandhamaddy007@gmail.com>
2021-12-08 21:19:41 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,9 +2,5 @@ import React from 'react';
 import Home from './components/Home';
 
 export default function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+  return <div className="App"></div>;
 }
