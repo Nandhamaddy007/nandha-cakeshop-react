2d0abc6d985eab5cce693e365fcb69c63aeec2f5
Nandhamaddy007 <nandhamaddy007@gmail.com>
2021-12-14 12:00:17 +0530

    1 file modified

--- src/components/AddCake.js
+++ src/components/AddCake.js
@@ -110,6 +110,7 @@ export default function AddCake(props) {
           className="float right"
           style={{ border: '5px solid black', float: 'right' }}
           alt="Your image preview"
+          name="image"
         />
         <input
           type="file"
@@ -203,16 +204,33 @@ export default function AddCake(props) {
         <div class="row">
           {cake.ingredients?.map((data, index) => {
             return (
-              <input
-                style={{ width: '200px' }}
-                type="text"
-                className="form-control m-1"
-                value={data}
-                name="ingredients"
-                onChange={getValue}
-                id={index}
-                key={index}
-              />
+              <div className="input-group  m-1" style={{ width: '200px' }}>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={data}
+                  name="ingredients"
+                  onChange={getValue}
+                  id={index}
+                  key={index}
+                />
+                <div class="input-group-append" style={{ width: '20px' }}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-trash"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                    <path
+                      fill-rule="evenodd"
+                      d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+                    />
+                  </svg>
+                </div>
+              </div>
             );
           })}{' '}
           <button
